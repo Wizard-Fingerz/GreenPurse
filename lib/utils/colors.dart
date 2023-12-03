60d1cceafa499600f4a00e57145b3ebb46297d68
2gedaFullStack <2gedafullstack@gmail.com>
2023-12-03 01:48:48 +0100

    added splashscreen

---
+++ lib/utils/colors.dart
@@ -0,0 +1,24 @@
+// colors.dart
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const MaterialColor customPrimaryColor = const MaterialColor(
+    0xFF23AA49,
+    <int, Color>{
+      50: Color(0xFFEAE3F9),
+      100: Color(0xFFC5A6E6),
+      200: Color(0xFF9E69D2),
+      300: Color(0xFF782CC0),
+      400: Color(0xFF6202A9),
+      500: Color(0xFF4E0CA2),
+      600: Color(0xFF460991),
+      700: Color(0xFF3E0681),
+      800: Color(0xFF350470),
+      900: Color(0xFF280350),
+    },
+  );
+
+  static const Color green = const Color(0xFF23AA49);
+  // Add more colors as needed
+}
